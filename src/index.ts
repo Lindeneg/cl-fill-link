b05134b641dd9c732e8b3848c52b1573715a4e35
lindeneg <christian@lindeneg.org>
2021-12-15 08:27:57 +0100

    remove mock code

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ type Obj = Record<string, unknown>;
 
 type ReplacerValue<T extends string, K extends unknown> = K extends string
   ? T extends `${infer U}[...${K}]`
-    ? string[]
+    ? [string, ...string[]]
     : T extends `${infer P}[[...${K}]]`
     ? string[]
     : string
@@ -49,37 +49,3 @@ export function fillLink<T extends string, K extends Obj>(
     return link;
   }
 }
-
-export enum links {
-  N404 = "/404",
-  N500 = "/500",
-  ADMIN = "/admin",
-  ADMIN_ADMINISTRATE = "/admin/administrate",
-  ADMIN_USER = "/admin/user",
-  ADMIN_USER_OPTIONS_DASHBOARD = "/admin/user/options/dashboard",
-  CUSTOMER_ID_CONTENT_SLUG = "/customer/[id]/content/[slug]",
-  CONTENT = "/content",
-  CONTENT_ARTICLEID = "/content/[articleId]",
-  POSTS_CATCHALL_SLUG = "/posts/[...slug]",
-  USER_OPTIONAL_CATCHALL_SLUG = "/user/[[...slug]]",
-}
-
-let l: ReplacerValue<"/customer/[id]/content/[...slug]", "slug">;
-let ll: Replacer<
-  "/customer/[id]/content/[...slug]",
-  { id: unknown; slug: unknown }
->;
-
-console.log(links.CUSTOMER_ID_CONTENT_SLUG);
-console.log(
-  fillLink(links.CUSTOMER_ID_CONTENT_SLUG, {
-    slug: "davis",
-  })
-);
-console.log();
-console.log(links.USER_OPTIONAL_CATCHALL_SLUG);
-console.log(
-  fillLink(links.USER_OPTIONAL_CATCHALL_SLUG, {
-    slug: ["hello", "there", "man"],
-  })
-);
