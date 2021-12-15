7b2c8e4f5951a3546331d9644b924816bf95b52c
lindeneg <christian@lindeneg.org>
2021-12-15 09:51:58 +0100

    implement tests

--- __tests__/catchall.test.ts
+++ __tests__/catchall.test.ts
@@ -1,6 +1,25 @@
 import { fillLink, fillLinkSafe } from "../src/index";
-import type { AppLinks } from "./shared";
+import { AppLink } from "./shared";
 
 describe("Catchall Test Suite", () => {
-  test("", () => {});
+  test.each([
+    ["safe", fillLinkSafe],
+    ["unsafe", fillLink],
+  ])("%s: can fill single catchall slug", (_, fn) => {
+    expect(
+      fn(AppLink.POSTS_CATCHALL_SLUG, {
+        slug: ["category"],
+      })
+    ).toEqual("/posts/category");
+  });
+  test.each([
+    ["safe", fillLinkSafe],
+    ["unsafe", fillLink],
+  ])("%s: can fill multiple catchall slugs", (_, fn) => {
+    expect(
+      fn(AppLink.POSTS_CATCHALL_SLUG, {
+        slug: ["category", "music", "jazz", "miles-davis"],
+      })
+    ).toEqual("/posts/category/music/jazz/miles-davis");
+  });
 });
--- __tests__/shared.ts
+++ __tests__/shared.ts
@@ -1,9 +1,7 @@
-export enum AppLinks {
+export enum AppLink {
   ADMIN_USER_ID = "/admin/user/[id]",
   ADMIN_USER_ID_DASHBOARD_VIEW = "/admin/user/[id]/dashboard/[view]",
-  CUSTOMER_ID_CONTENT_VIEW = "/customer/[id]/content/[view]",
   OPTIONAL_CATCHALL_SLUG = "/[[...slug]]",
-  CONTENT_ARTICLEID = "/content/[articleId]",
   POSTS_CATCHALL_SLUG = "/posts/[...slug]",
   USER_OPTIONAL_CATCHALL_SLUG = "/user/[[...slug]]",
 }
