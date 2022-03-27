a4bef1dd4943e5ab0e2e8e0007e8cd75d8f9512a
lindeneg <christian@lindeneg.org>
2022-03-27 11:56:13 +0200

    accept a $query property in the Replacer obj

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,31 @@ type ReplacerValue<T extends string, K extends unknown> = K extends string
 
 type Replacer<T extends string, U extends Obj> = {
   [K in keyof U]: ReplacerValue<T, K>;
-};
+} & Query;
+
+function getString(target: unknown): string {
+  try {
+    return String(target);
+  } catch (_) {
+    return '';
+  }
+}
+
+function appendQuery<T extends Query>(l: string, query?: T): string {
+  let q = '?';
+  if (query) {
+    const keys = Object.keys(query);
+    const length = keys.length - 1;
+    keys.forEach((key, idx) => {
+      const val = getString(query[<keyof T>key]);
+      q += key + '=' + val;
+      if (idx < length) {
+        q += '&';
+      }
+    });
+  }
+  return l + q;
+}
 
 export function fillLinkSafe<T extends string, K extends Obj>(
   link: T,
