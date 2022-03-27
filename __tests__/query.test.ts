6f8dfaa973c117d15604605147da6a7c4fd6c8f6
lindeneg <christian@lindeneg.org>
2022-03-27 13:46:37 +0200

    initial test

--- __tests__/query.test.ts
+++ __tests__/query.test.ts
@@ -1,6 +1,19 @@
-import { fillLink, fillLinkSafe } from "../src/index";
+import { fillLink, fillLinkSafe } from '../src/index';
 
-describe("Query Test Suite", () => {
-  test("", () => {
+describe('Query Test Suite', () => {
+  test('safe: can fill single key with $query', () => {
+    expect(
+      fillLinkSafe('/admin/user/[id]', {
+        id: 1,
+        $query: {
+          music: 'jazz',
+          artist: 'miles-davis',
+          tune: 'so-what',
+          year: 1967,
+        },
+      })
+    ).toEqual(
+      '/admin/user/1?music=jazz&artist=miles-davis&tune=so-what&year=1967'
+    );
   });
 });
