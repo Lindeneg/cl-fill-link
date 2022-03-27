518c40d6680aecbdf14cd79d566acd0e51b8b682
lindeneg <christian@lindeneg.org>
2022-03-27 14:08:01 +0200

    jest should use test config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   roots: ["<rootDir>"],
   testMatch: ["**/__tests__/**/*.test.+(ts|tsx|js)"],
+  globals: {
+    'ts-jest': {
+      tsconfig: './tsconfig-test.json',
+    },
+  },
   transform: {
     "^.+\\.(ts|tsx)$": "ts-jest",
   },
